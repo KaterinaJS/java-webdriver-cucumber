82ceecf0c6483fe8fb560ac29789edab36b00433
KaterinaJS <ekaterinatrukhanova@gmail.com>
2020-08-23 15:33:25 -0700

    Add usps scenario "calculate the price"

--- src/test/java/definitions/UspsStepDefs.java
+++ src/test/java/definitions/UspsStepDefs.java
@@ -5,6 +5,10 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static support.TestContext.getDriver;
@@ -32,4 +36,30 @@ public class UspsStepDefs {
         assertThat(resultForm.contains(zip));
     }
 
+    @When("I go to Calculate Price Page")
+    public void iGoToCalculatePricePage() {
+        WebElement mailAndShip = getDriver().findElement(By.xpath("//a[@id='mail-ship-width']"));
+        Actions actions = new Actions(getDriver());
+        actions.moveToElement(mailAndShip).perform();
+        getDriver().findElement(By.xpath("//li[@class='tool-calc']/a[contains(@href,'postcalc')]")).click();
+    }
+
+    @And("I select {string} with {string} shape")
+    public void iSelectWithShape(String country, String shape) {
+        Select countrySelect = new Select(getDriver().findElement(By.xpath("//select[@id='CountryID']")));
+        countrySelect.selectByVisibleText(country);
+        getDriver().findElement(By.xpath("//input[@value='" + shape + "']")).click();
+    }
+
+    @And("I define {string} quantity")
+    public void iDefineQuantity(String number) {
+        getDriver().findElement(By.xpath("//input[@placeholder='Quantity']")).sendKeys(number);
+    }
+
+    @Then("I calculate the price and validate cost is {string}")
+    public void iCalculateThePriceAndValidateCostIs(String cost) {
+        getDriver().findElement(By.xpath("//input[@value='Calculate']")).click();
+        String actualCost = getDriver().findElement(By.xpath("//div[@id='cost-0']")).getText();
+        assertThat(actualCost.equals(cost));
+    }
 }
--- src/test/resources/features/usps.feature
+++ src/test/resources/features/usps.feature
@@ -7,3 +7,11 @@ Feature: USPS scenarios
     When I go to Lookup ZIP page by address
     And I fill out "4970 El Camino Real" street, "Los Altos" city, "CA" state
     Then I validate "94022" zip code exists in the result
+
+  @usps2
+  Scenario: Calculate price
+    Given I go to "usps" page
+    When I go to Calculate Price Page
+    And I select "Canada" with "Postcard" shape
+    And I define "2" quantity
+    Then I calculate the price and validate cost is "$2.40"
